dfc8fa9728abe55feb76acf557e63b9a6fee6633
Mikhail Matiunin <mikhail.matiunin@cityplumbing.co.uk>
2022-12-29 18:00:21 +0400

    Replace EncryptionAlgorithm with enum

--- lib/src/feature/encryption/data/encryption_algorithm.dart
+++ lib/src/feature/encryption/data/encryption_algorithm.dart
@@ -1,68 +1,16 @@
-import 'package:meta/meta.dart';
+enum EncryptionAlgorithm {
+  aesSync('Synchronous AES encryption', 'Affects the event loop', _$algorithm),
+  aesAsync('Asynchronous AES encryption', 'Does not affect the event loop', _$algorithm),
+  aesIsolate('Isolated AES encryption', 'Performed in a separate isolator', _$algorithm);
 
-@immutable
-abstract class EncryptionAlgorithm {
-  const EncryptionAlgorithm();
-
-  static const EncryptionAlgorithm aesSync = EncryptionAlgorithmAESSync();
-  static const EncryptionAlgorithm aesAsync = EncryptionAlgorithmAESAsync();
-  static const EncryptionAlgorithm aesIsolate = EncryptionAlgorithmAESIsolate();
-
-  static const List<EncryptionAlgorithm> values = <EncryptionAlgorithm>[
-    aesSync,
-    aesAsync,
-    aesIsolate,
-  ];
-
-  abstract final String name;
-  abstract final String description;
+  const EncryptionAlgorithm(this.name, this.description, this._encrypt);
 
   static Future<void> encrypt(EncryptionAlgorithm algorithm) => algorithm._encrypt();
 
-  Future<void> _encrypt();
+  final String name;
+  final String description;
 
-  @override
-  String toString() => name;
+  final Future<void> Function() _encrypt;
 }
 
-@sealed
-class EncryptionAlgorithmAESSync extends EncryptionAlgorithm {
-  const EncryptionAlgorithmAESSync();
-
-  @override
-  String get name => 'Synchronous AES encryption';
-
-  @override
-  String get description => 'Affects the event loop';
-
-  @override
-  Future<void> _encrypt() async {}
-}
-
-@sealed
-class EncryptionAlgorithmAESAsync extends EncryptionAlgorithm {
-  const EncryptionAlgorithmAESAsync();
-
-  @override
-  String get name => 'Asynchronous AES encryption';
-
-  @override
-  String get description => 'Does not affect the event loop';
-
-  @override
-  Future<void> _encrypt() => const EncryptionAlgorithmAESSync()._encrypt();
-}
-
-@sealed
-class EncryptionAlgorithmAESIsolate extends EncryptionAlgorithm {
-  const EncryptionAlgorithmAESIsolate();
-
-  @override
-  String get name => 'Isolated AES encryption';
-
-  @override
-  String get description => 'Performed in a separate isolator';
-
-  @override
-  Future<void> _encrypt() => const EncryptionAlgorithmAESSync()._encrypt();
-}
+Future<void> _$algorithm() async {}
